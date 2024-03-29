b6037c0a9b6bd0ab2055cc2b51f7fb1f9c068ae0
dyerlab <rjdyer@vcu.edu>
2024-03-29 12:06:14 -0400

    Working on histogram stuff

---
+++ Sources/PresentationZen/Charts/Histogram.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Rodney Dyer on 3/29/24.
+//
+
+import SwiftUI
+
+struct Histogram: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Histogram()
+}
