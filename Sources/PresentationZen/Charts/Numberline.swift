14208acb352272e9200ed4c79b2a5422d97d398a
Rodney Dyer <rodney@dyerlab.com>
2024-11-03 09:50:41 -0500

    Adding web page support for this.

---
+++ Sources/PresentationZen/Charts/Numberline.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  PresentationZen
+//
+//  Created by Rodney Dyer on 11/1/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
